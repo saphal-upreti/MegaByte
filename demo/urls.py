353914c4b3dc25264c5434b07b3357efe86eebf0
saphal-upreti <saphalupreti95@gmail.com>
2022-07-10 21:56:01 +0545

    initial commit

--- demo/urls.py
+++ demo/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 
 from django.views.static import serve
-from django.conf.urls import url
+
 
 admin.site.site_header="MegaByte"
 admin.site.site_title="Admin"
