9cae87f11164a444fb4706c8d1695d290238f657
saphal-upreti <108993335+saphal-upreti@users.noreply.github.com>
2022-12-08 11:33:36 +0545

    Update login.js

--- static/login.js
+++ static/login.js
@@ -1,19 +1,54 @@
 login=()=>{
+    var form = document.getElementById('form')
+    var username = document.getElementById('username').value
     var characters = ['!','@',"#","$","%","^","&","*","(",")","_","-","+","=","`","~","'",'"',"[","{","}","]",":",";","|",'/',"?","<",",",">","."]
-    var username=document.getElementById('username')
-    var username_value = username.value
-    /*for (let i=0;i<username_value.length;i++){
-        for(let j=0;j<characters.length;j++){
-            if(username_value[i]!=characters[j]){
-                return true
+    var characters_length = characters.length
+    var username_length = username.length
+    for(let i=0;i<username_length;i++){
+        for(var j=0;j<characters_length;j++){
+            if(username[i]==characters[j]){
+                var check=1;
+                break;
             }
             else{
-                return false
+                var check=0;
             }
-            
         }
-    }*/
-    console.log(username_value)
+        if(check==1){
+            break
+        }
+    }
+    if(check==1){
+        document.getElementById('error').innerHTML = 'invalid character '+characters[j]+' !'
+        return false
+    }
+    else{
+        document.getElementById('error').innerHTML=''
+        return true
+    }
 }
-
+password_check=()=>{
+    var num = [0,1,2,3,4,5,6,7,8,9]
+    var password = document.getElementById('password').value
+    for(let i=0; i<password.length; i++){
+        for(let j=0; j<num.length; j++){
+            if(password[i]==num[j]){
+                var check = 1
+                break
+            }
+        }
+        if(check==1){
+            break
+        }
+    
+    }
+    if(check==1){
+        document.getElementById('password_error').innerHTML=''
+        return true
+    }
+    else{
+        document.getElementById('password_error').innerHTML='at least one number needed !'
+        return false
+    }
+}
 
